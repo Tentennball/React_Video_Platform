a0f1be746720b80159fb9b34f22160db46b613bc
ches0703 <ches0703@naver.com>
2023-08-16 19:27:58 +0900

    [Code] Test용 코드 생성

--- videoplatform/src/App.js
+++ videoplatform/src/App.js
@@ -1,2 +1,14 @@
+import Container from '@mui/material/Container';
 import React from 'react';
+import MovieList from './Component/MovieList';
 
+
+function App() {
+  return (
+    <Container maxWidth="lg" disableGutters={true} sx={{backgroundColor: "#D9D9D9"}}>
+      <MovieList></MovieList>
+    </Container>
+  );
+}
+
+export default App
