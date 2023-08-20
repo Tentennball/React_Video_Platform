d40983fc2d2637257165bf650d22afdc7edc2a8f
ches0703 <ches0703@naver.com>
2023-08-20 22:26:13 +0900

    [Code] Modal 코드 추가

--- videoplatform/src/Component/VideoModal.js
+++ videoplatform/src/Component/VideoModal.js
@@ -2,7 +2,10 @@ import {
   Container,
   Box,
   Modal,
+  Avatar,
+  Typography,
 } from '@mui/material';
+
 import ReactPlayer from 'react-player'
 const VideoModal = ({handleClose}) => {
   return (
@@ -33,10 +36,12 @@ const VideoModal = ({handleClose}) => {
               backgroundColor: "#000000", 
             }}>
               <ReactPlayer
-                    url={'https://firebasestorage.googleapis.com/v0/b/todoapp-3a3c7.appspot.com/o/Video%2FIVE.mp4?alt=media&token=89ee5627-9381-41ed-95e9-d15164ca3cfe'}    // 플레이어 url
+                    //url={'https://firebasestorage.googleapis.com/v0/b/todoapp-3a3c7.appspot.com/o/Video%2FIVE.mp4?alt=media&token=89ee5627-9381-41ed-95e9-d15164ca3cfe'}    // 플레이어 url
+                    url={`/img/IVE.mp4`}
+                    volume = {0.5}
                     width='100%'         
                     height='100%'        
-                    playing={true}        
+                    //playing={true}        
                     controls={true}       
                 />
             </Box>
@@ -46,11 +51,28 @@ const VideoModal = ({handleClose}) => {
               width: "100%", 
               height: "100px", 
               backgroundColor: "#585858", 
+              padding: "20px",
+              boxSizing: "border-box",
+              display: "flex"
             }}>
-              Title
+              {/* Profile Img */}
+              <Avatar alt="Cindy Baker" src="/img/An.jpg" variant="rounded" sx={{width: "60px", height: "60px"}} />
+              {/* Title & Uploader Name */}
+              <Box sx={{marginLeft: "20px", width:"100%",  overflow:"hidden", backgroundColor: "black"}}>
+              <Typography variant="h6" sx={{color: "#FFFFFF", whiteSpace: "nowrap"}}>
+                This is Test Title
+              </Typography>
+              <Typography variant="subtitle1" sx={{color: "#FFFFFF", whiteSpace: "nowrap"}}>
+                Uploader Name
+              </Typography>
+
+              </Box>
+              <Box sx={{marginLeft: "20px", width:"25vw",  overflow:"hidden", backgroundColor: "black"}}></Box>
+
+
             </Box>
           </Box>
-        </Container>
+        </Container>  
       </Modal>
   );
 }
