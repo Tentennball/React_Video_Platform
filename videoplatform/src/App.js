db1bc4cd1b49fd5aa70322ecf42b35bd735c2a6c
ches0703 <ches0703@naver.com>
2023-08-21 13:33:33 +0900

    [Code] Theme 추가

--- videoplatform/src/App.js
+++ videoplatform/src/App.js
@@ -1,16 +1,27 @@
 import Container from '@mui/material/Container';
 import React from 'react';
-
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import VideoList from './Component/VideoList';
 import Navbar from './Navbar';
+
+const theme = createTheme({
+  palette: {
+    white: {
+      main: "#ffffff"
+    }
+  }
+});
+
+
+
 function App() {
   return (
-    <div>
+    <ThemeProvider theme={theme}>
     <Navbar/>
     <Container maxWidth="lg"  sx={{backgroundColor: "#1F1F1F"}}>
       <VideoList></VideoList>
     </Container>
-    </div>
+    </ThemeProvider>
   );
 }
 
