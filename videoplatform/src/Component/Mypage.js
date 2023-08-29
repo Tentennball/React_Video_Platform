141f31aeada28be2c21d14c5f41709205544b2f8
tentenball <lth8905@gmail.com>
2023-08-30 01:10:00 +0900

    mypage rendering

--- videoplatform/src/Component/Mypage.js
+++ videoplatform/src/Component/Mypage.js
@@ -1,5 +1,12 @@
-import React from "react";
+import Container from "@mui/material/Container";
+const Mypage = () => {
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{ backgroundColor: "#1F1F1F", marginTop: "4rem" }}
+    >
+    </Container>
+  );
+};
 
-const Mypage = () =>{
-    
-}
+export default Mypage;
