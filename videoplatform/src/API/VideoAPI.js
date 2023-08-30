aee2c78df370210e11d3dd2c2b5f9e911ae1b5c1
ches0703 <ches0703@naver.com>
2023-08-30 19:53:19 +0900

    [Code] API Code Upgrade

--- videoplatform/src/API/VideoAPI.js
+++ videoplatform/src/API/VideoAPI.js
@@ -2,11 +2,29 @@ import { storage, store } from "../firebase";
 import { doc, getDocs, setDoc } from "firebase/firestore";
 import { collection, query, where } from "firebase/firestore";
 
+// VideoUpload시 DB에 Video의 메타 데이터를 저장하는 API
+export const uploadVideoDataApi = async (VideoData) => {
+  await setDoc(doc(store, "VideoList", VideoData.id), VideoData)
+    .then(() => {console.log("Upload Video Data is Success")})
+    .catch((e) => { console.error(e); alert("Upload Video Data Api Fail")});
+};
 
-export const uploadVideoData = async (VideoData) => {
-  await setDoc(doc(store, "VideoList", VideoData.id), VideoData).catch((e) => {
-    console.log(e);
-  });
+
+// HomePage에서 VideoList를 받아오는 API
+export const getVideoListApi = async () => {
+  return await getDocs(query(collection(store, "VideoList")))
+    .then((snapShot) => {
+      const videoList = [];
+      snapShot.forEach((doc) => {
+        videoList.push(doc.data());
+      });
+      return videoList
+    })
+    .catch((e) => {
+      console.error(e)
+      alert("Get Video List Api Fail")
+      return []
+    });
 };
 
 //여기 userName
@@ -22,15 +40,6 @@ export const getUserVideoList = async (setVideoList, userName) => {
     else{
       return;
     }
-  });
-  setVideoList(videoList);
-};
-export const getVideoList = async (setVideoList) => {
-  const videoList = [];
-  const videoListQuery = query(collection(store, "VideoList"));
-  const snapShot = await getDocs(videoListQuery);
-  snapShot.forEach((doc) => {
-    videoList.push(doc.data());
   });
   setVideoList(videoList);
 };
