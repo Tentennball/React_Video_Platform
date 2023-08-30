8b827e4d90ccd270c0bcb9b75dc305dcfe42231a
ches0703 <ches0703@naver.com>
2023-08-30 20:02:07 +0900

    [File] MyPage.js 파일 위치 이동

--- videoplatform/src/index.js
+++ videoplatform/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import Mypage from './Component/Mypage';
+import Mypage from './Mypage';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
