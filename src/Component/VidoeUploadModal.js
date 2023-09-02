a2fe6872bfac0b511e3cb62ce589bc05925b6ac4
tentenball <lth8905@gmail.com>
2023-09-02 22:59:05 +0900

    Edit

--- src/Component/VidoeUploadModal.js
+++ src/Component/VidoeUploadModal.js
@@ -14,6 +14,7 @@ import { useSelector } from "react-redux";
 
 const VideoUploadModal = (props) => {
   const [videoProgress, setVideoProgress] = useState(0);
+  // eslint-disable-next-line no-unused-vars
   const [thumbProgress, setThumbProgress] = useState(0);
   const [videoFile, setVideoFile] = useState(null);
   const [thumbnailFile, setThumbnailFile] = useState(null);
